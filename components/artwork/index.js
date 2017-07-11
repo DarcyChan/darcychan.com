c4770f51602965dfad8d9f4a9df768b17304c766
Darcy Chan <darcy.chan@atmist.com>
2017-07-11 14:35:03 -0600

    Fixed a typo that was causing build errors.

--- components/artwork/index.js
+++ components/artwork/index.js
@@ -2,4 +2,4 @@ export { default as Artwork } from 'components/artwork/artwork';
 export { default as ArtworkBrand } from 'components/artwork/artworkBrand';
 export {
     default as ArtworkInfoBlock
-} from 'components/artwork/ArtworkInfoBlock';
+} from 'components/artwork/artworkInfoBlock';
